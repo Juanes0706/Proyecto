a576d7607768a857b2df05f952001f59358c0893
Juan Esteban Tovar <jetovar50@ucatolica.edu.co>
2025-05-12 08:53:19 -0500

    Proyecto corregido, no encontraba la base de datos

--- db.py
+++ db.py
@@ -1,26 +1,15 @@
-import os
-from dotenv import load_dotenv
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from databases import Database
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
 
-load_dotenv()
-
+# Obtener la URL de la base de datos desde una variable de entorno
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-if DATABASE_URL is None:
+if not DATABASE_URL:
     raise ValueError("La variable de entorno DATABASE_URL no está definida")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-database = Database(DATABASE_URL)
-
 Base = declarative_base()
-
-async def connect_db():
-    await database.connect()
-
-async def disconnect_db():
-    await database.disconnect()
