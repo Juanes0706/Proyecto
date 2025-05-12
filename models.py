d941c514c1dd2cbf3a34fb386721fac18362e869
Juan Esteban Tovar <jetovar50@ucatolica.edu.co>
2025-05-12 09:29:38 -0500

    cambio en models y correcion de errores

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
 from db import Base
 
 class Ruta(Base):
@@ -8,6 +9,20 @@ class Ruta(Base):
     tipo_servicio = Column(String)
     horario = Column(String)
     activo = Column(Boolean, default=True)
+
+    buses = relationship("Bus", back_populates="ruta")
+
+class Bus(Base):
+    __tablename__ = "buses"
+    id = Column(Integer, primary_key=True, index=True)
+    placa = Column(String, unique=True, index=True)
+    modelo = Column(String)
+    capacidad = Column(Integer)
+    tipo = Column(String)  # troncal o zonal
+    activo = Column(Boolean, default=True)
+
+    ruta_id = Column(Integer, ForeignKey("rutas.id"))
+    ruta = relationship("Ruta", back_populates="buses")
 
 class Estacion(Base):
     __tablename__ = "estaciones"
