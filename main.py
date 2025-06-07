a2df1eb1dfb33000ae38fc5e427656a6d95d6c1b
Juan Esteban Tovar <jetovar50@ucatolica.edu.co>
2025-06-06 22:23:26 -0500

    Arreglos finales

--- main.py
+++ main.py
@@ -7,6 +7,14 @@ import home
 
 app = FastAPI()
 
+@app.on_event("startup")
+async def on_startup():
+    await create_db_tables() # Llama a la función para crear las tablas al iniciar
+
+async def create_db_tables():
+    async with async_engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 app.include_router(home.router)
