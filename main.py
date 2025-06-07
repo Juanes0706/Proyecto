cbb5391df9e90029e6f0ba7442e54f079be6582f
Juan Esteban Tovar <jetovar50@ucatolica.edu.co>
2025-06-06 20:32:49 -0500

    Cambio DB

--- main.py
+++ main.py
@@ -2,20 +2,19 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.requests import Request
-
-from app.db import engine  # ajusta la ruta si es diferente
-from app.models import Base  # ajusta la ruta según donde estén tus modelos
+from app.database.db import  async_engine, Base
 
 import home
 
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
-app.include_router(home.router)
-templates = Jinja2Templates(directory="templates")
 
+app.include_router(home.router)
+
+templates = Jinja2Templates(directory="templates")
 
 @app.on_event("startup")
 async def startup():
-    async with engine.begin() as conn:
+    async with async_engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
